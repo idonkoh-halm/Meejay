37cada0b13b9647f7da235ac0735b871bbc773aa
Isaac Donkoh-Halm <isaac.donkoh-halm@innovationcharter.org>
2016-03-04 13:17:27 -0500

    new direction, create txt from user input

---
+++ linkcreator.py
@@ -0,0 +1,41 @@
+import sys
+import urllib2
+
+class Text_Output:
+    '''
+    from a bunch of user defined links, this will attempt to take those links
+    and add them line by line to create a m3u file.
+    '''
+    def ___init___(self,filename):
+        self.f = file(filename,'w')
+    def new_text (self, txt):
+        self.f.write(txt+'\n') #creates a new line
+    def output_list(self, url_list):
+        txt=''
+        for itm in data_list:
+            txt+= str(itm)+'\n'
+            
+    
+    def finish(self):
+        self.f.close()
+
+def m3u_generator(filename='playlist.txt'):
+    def texttohtml():
+        directory="http://googledrive.com/host/0B_QRZ8n8sCFLdkJTMU1Ed0k3VVk/music/JetSetRadio/"
+        text=raw_input('Enter normie text here')
+        newlink= directory+urllib2.quote(text)+".mp3"
+        print newlink
+        return newlink
+    text_output=Text_Output(filename)
+    text_output.new_text(newlink)
+
+m3u_generator()
+
+def texttohtml():
+    directory="http://googledrive.com/host/0B_QRZ8n8sCFLdkJTMU1Ed0k3VVk/music/JetSetRadio/"
+    text=raw_input('Enter normie text here')
+    newlink= directory+urllib2.quote(text)+".mp3"
+    print newlink
+    return newlink
+
+texttohtml()
